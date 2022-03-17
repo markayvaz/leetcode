4ceacb70388810788fb20650298311d250b72666
Mark Ayvaz <mark.ayvaz98@gmail.com>
2022-03-17 15:31:01 +0400

    Update Roman to Integer.js

--- problems/Roman to Integer.js
+++ problems/Roman to Integer.js
@@ -2,6 +2,22 @@
  * @param {string} s
  * @return {number}
  */
- var romanToInt = function(s) {
-    
+var romanToInt = function (s) {
+  const symbolCount = s.length;
+
+  let symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  const map = new Map();
+
+  for (let i = 0; i < symbolCount; i++) {
+      
+  }
 };
