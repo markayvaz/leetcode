6a475353272982273ae4b6b678c44af4583ca1bc
Mark Ayvaz <mark.ayvaz98@gmail.com>
2022-03-18 12:06:53 +0400

    Update Roman to Integer.js

--- problems/Roman to Integer.js
+++ problems/Roman to Integer.js
@@ -6,21 +6,27 @@ var romanToInt = function (s) {
   const symbolCount = s.length;
   let res = 0;
 
-  let symbols = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
+  const symbols = new Map();
+
+  symbols.set("I", 1);
+  symbols.set("V", 5);
+  symbols.set("X", 10);
+  symbols.set("L", 50);
+  symbols.set("C", 100);
+  symbols.set("D", 500);
+  symbols.set("M", 1000);
 
   const map = new Map();
 
   for (let i = 0; i < symbolCount; i++) {
-        char = s.charAt(i);
+    char = s.charAt(i);
+
+    val = symbols.get(char);
+
+    res += val;
   }
+
+  return res;
 };
 
 let res = romanToInt("IIII");
