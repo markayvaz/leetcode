5d2083af7971436e94ff0a3bb08ccd00776af621
Mark Ayvaz <mark.ayvaz98@gmail.com>
2022-04-07 07:36:24 +0800

    Solved Add Two Numbers

--- problems/Add Two Numbers.js
+++ problems/Add Two Numbers.js
@@ -1,16 +1,43 @@
-//  Definition for singly-linked list.
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val;
-  this.next = next === undefined ? null : next;
-}
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  
-};
+  let list = new ListNode();
+  let head = list;
+  let sum = 0;
 
-console.log(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]));
+  while (l1 || l2 || sum > 0) {
+    let carry = false;
+
+    if (l1) {
+      sum = sum + l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2) {
+      sum = sum + l2.val;
+      l2 = l2.next;
+    }
+
+    if (sum >= 10) {
+      carry = true;
+      sum = sum - 10;
+    }
+
+    head.next = new ListNode(sum);
+    head = head.next;
+
+    carry ? (sum = 1) : (sum = 0);
+  }
+
+  return list.next;
+};
