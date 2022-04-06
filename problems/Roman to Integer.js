b15e78685e13a81b01af6794849d461fa0141b58
Mark Ayvaz <mark.ayvaz98@gmail.com>
2022-04-07 06:05:24 +0800

    Solved Roman to Integer

--- problems/Roman to Integer.js
+++ problems/Roman to Integer.js
@@ -3,32 +3,36 @@
  * @return {number}
  */
 var romanToInt = function (s) {
-  const symbolCount = s.length;
   let res = 0;
 
   const symbols = new Map();
 
   symbols.set("I", 1);
+  symbols.set("IV", 4);
   symbols.set("V", 5);
+  symbols.set("IX", 9);
   symbols.set("X", 10);
+  symbols.set("XL", 40);
   symbols.set("L", 50);
+  symbols.set("XC", 90);
   symbols.set("C", 100);
+  symbols.set("CD", 400);
   symbols.set("D", 500);
+  symbols.set("CM", 900);
   symbols.set("M", 1000);
 
-  const map = new Map();
-
-  for (let i = 0; i < symbolCount; i++) {
-    char = s.charAt(i);
-
-    val = symbols.get(char);
-
-    res += val;
+  for (let i = 0; i < s.length; i++) {
+    if (i + 1 < s.length && symbols.has(`${s[i]}${s[i + 1]}`)) {
+      console.log(`Read: ${s[i]}${s[i + 1]}`);
+      res += symbols.get(`${s[i]}${s[i + 1]}`);
+      i++;
+    } else {
+      console.log(`Read: ${s[i]}`);
+      res += symbols.get(s[i]);
+    }
   }
 
   return res;
 };
 
-let res = romanToInt("IIII");
-
-console.log(res);
+console.log(romanToInt("MCMXCIV"));
