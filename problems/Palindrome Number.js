a00ee53fa3a0e912cf48151e8f049030f797a766
Mark Ayvaz <mark.ayvaz98@gmail.com>
2022-04-07 06:05:34 +0800

    Solved Palindrome Number

--- problems/Palindrome Number.js
+++ problems/Palindrome Number.js
@@ -4,5 +4,14 @@
  */
 
 var isPalindrome = function (x) {
-    
+  const x2 = parseInt(x.toString().split("").reverse().join(""));
+
+  if (x === x2 || x / x2 === 1) {
+    return true;
+  }
+
+  return false;
 };
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
