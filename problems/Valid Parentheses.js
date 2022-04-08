04903121f78c47ff2dd2de3fb0a7c6c3c7f96b90
Mark Ayvaz <mark.ayvaz98@gmail.com>
2022-04-08 13:07:51 +0800

    Solved Valid Parentheses

--- problems/Valid Parentheses.js
+++ problems/Valid Parentheses.js
@@ -2,6 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
- var isValid = function(s) {
-    
+ var isValid = function(s) {  
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        const c = s.charAt(i);
+        switch(c) {
+            case '(':
+                stack.push(')');
+                break;
+            case '{':
+                stack.push('}');
+                break;
+            case '[':
+                stack.push(']');
+                break;
+            default:              
+            if (c !== stack.pop()) {
+                return false;
+            }
+        }        
+    }
+        
+    return stack.length === 0;
 };
